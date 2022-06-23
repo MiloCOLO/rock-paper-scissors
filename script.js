6c4365900ba410ca22403cded667d23f1344841c
marzec <gregmarzec02@gmail.com>
2022-06-23 15:20:25 -0400

    needs to be fixed

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ var win = 0;
 var loss = 0;
 var tie = 0;
 var choice = ["r", "p", "s"];
+var compChoice;
+var userChoice; 
 
 function winGame() {
-    if (userChoice = "r" && compChoice == "s") {
+    //compChoice = "s"
+    console.log("This is where comp is " + compChoice)
+    if (userChoice === "r" && compChoice === "s") {
         alert ("you've won");
         var win = win + 1;
         return
-    } else if (userChoice = "p" && compChoice == "r") {
+    } else if (userChoice === "p" && compChoice === "r") {
         alert ("you've won");
         var win = win + 1;
         return
-    } else if (userChoice = "s" && compChoice == "p") {
+    } else if (userChoice === "s" && compChoice === "p") {
         alert ("you've won");
         var win = win + 1;
         return
@@ -20,25 +24,25 @@ function winGame() {
 }
 
 function loseGame() {
-    if (userChoice = "s" && compChoice == "r") {
+    if (userChoice === "s" && compChoice === "r") {
         alert ("you've lost :(");
         var loss = loss + 1;
         return
-    } else if (userChoice = "r" && compChoice == "p") {
+    } else if (userChoice === "r" && compChoice === "p") {
         alert ("you've lost :(");
         var loss = loss + 1;
         return
-    } else if (userChoice = "p" && compChoice == "s") {
+    } else if (userChoice === "p" && compChoice === "s") {
         alert ("you've lost :(");
         var loss = loss + 1;
         return
     }
 }
 
-function tie() {
+function tieGame() {
     if (userChoice === compChoice) {
         alert ("TIE")
-        var tie = tie +1;
+        var tie = tie + 1;
         return
     }
 }
@@ -49,24 +53,23 @@ function restart() {
     var tie = 0;
 }
 
-function start() {
-    var userChoice=prompt("Please choose r, p, or s");
-    if (userChoice = "r" || "p" || "s") {
-        playGame();
-    }
-    // if (userChoice != "r" || userChoice != "p" || userChoice != "s") {
-    //     alert("please pick r,p,s");
-    // }
-
-    var compChoice =  choice[Math.floor(choice.length * Math.random())];
-}
-
 function playGame() {
     winGame();
     loseGame();
-    tie();
-
+    tieGame();
+    console.log("Wins: " + win);
+    console.log("Losses: " + loss);
+    console.log("Ties: " + tie);
 }
 
+function start() {
+    userChoice=prompt("Please choose r, p or s");
+    var compChoice =  choice[Math.floor(choice.length * Math.random())];
+    if (userChoice === "r" || userChoice === "p" || userChoice === "s") {
+        console.log(userChoice);
+        console.log(compChoice);
+        playGame();
+    }
+}
 
 start();
